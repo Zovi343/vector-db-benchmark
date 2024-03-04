4412a1139247faad8f25ff99acb9213b9a9262e7
Jakub Zovak <jakub.zovak@datasentics.com>
2024-03-05 00:15:14 +0100

    Just run filtered search through lvd script

--- start_lvd.sh
+++ start_lvd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python run.py --engines "lvd-model-mlp4-ncat-20-epoch-200-lrs-001-nbuck-2-bthr-02-constw--1" --datasets "hnm_2k" --host "lvd" --expname "hnm_2k" --kube
+python run.py --engines "lvd-model-mlp4-ncat-20-epoch-200-lrs-001-nbuck-2-bthr-02-constw--1" --datasets "hnm_no_filters" --host "lvd" --expname "results_hnm_bench_lvd" --kube
