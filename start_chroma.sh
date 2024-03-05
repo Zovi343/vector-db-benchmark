5d0dad823f2b2f37333affb571c1d4b00bf5b5c2
Jakub Zovak <jakub.zovak@datasentics.com>
2024-03-05 13:54:08 +0100

    Add bast tag to scripts

--- start_chroma.sh
+++ start_chroma.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 python run.py --engines "chroma-m-16-ef-100" --datasets "hnm_2k" --host "chroma" --expname "hnm_2k_second" --kube
--- start_weaviate.sh
+++ start_weaviate.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 python run.py --engines "weaviate-m-16-ef-128" --datasets "hnm_2k" --host "weaviate" --expname "hnm_2k" --kube
